281aed1167f40708a642b60b2dcf0537ac9383cb
Rasmus Kivi <rallejansson37@gmail.com>
2026-02-13 17:34:50 +0100

    init2

---
+++ terraform/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~>0.93.0"
+    }
+    talos = {
+      source  = "siderolabs/talos"
+      version = "0.10.1"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint  = "TBD"
+  api_token = "TBD"
+  insecure  = true
+}
+
+provider "talos" {}
